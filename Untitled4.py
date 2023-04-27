223f79a4fa2017704697a3fc5fbeab0d5c55226b
momii69 <131962405+momii69@users.noreply.github.com>
2023-04-27 15:15:40 +0500

    Add files via upload
    Iris

---
+++ Untitled4.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[17]:
+
+
+iris=pd.read_csv("C:\\Users\\My Pc\\Downloads\\iris.csv")
+
+
+# In[14]:
+
+
+iris
+
+
+# In[25]:
+
+
+iris.plot.scatter("SepalLengthCm","Species")
+plt.grid()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
